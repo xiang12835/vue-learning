493a04b1a3ab6499f72fa4227e81f956201f59ab
yu.xiang <yu.xiang@alibaba-inc.com>
2019-04-26 09:10:13 +0800

    add_vue_cli

---
+++ L05/my-app/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+    <div id="app">
+        {{msg}}
+        <div>
+            <input type="text" v-model="info">
+            <button @click="handleClick">添加</button>
+        </div>
+        <ul>
+            <!--<li v-for="item in list">{{item}}</li>-->
+
+            <!--组件-->
+            <!--<todo-item v-for="item in list" :item="item"></todo-item>-->
+
+            <!--插槽-->
+            <todo-item v-for="item in list" :key="item">
+
+                <!--普通插槽-->
+                <!--<span solt="item" style="font-size: 20px;">{{item}}</span>-->
+
+                <!--具名插槽-->
+                <!--<template v-slot:item>-->
+                    <!--<span style="font-size: 20px;">{{item}}</span>-->
+                <!--</template>-->
+
+                <!--作用域插槽-->
+                <template v-slot:item="itemProps">
+                    <span :style="{fontSize: '20px', color: itemProps.checked ? 'red' : 'blue'}">{{item}}</span>
+                </template>
+
+            </todo-item>
+
+
+        </ul>
+    </div>
+</template>
+
+<script>
+    import TodoItem from './components/TodoItem.vue'
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                msg: 'Hello Vue!',
+                info: '',
+                list: []
+            }
+        },
+        methods: {
+            handleClick() {
+                this.list.push(this.info);
+                this.info = "";
+            }
+        },
+        components: {
+            TodoItem
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ L05/my-app/src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li class='item'>
+    <input type="checkbox" v-model="checked">
+    <slot name="item" v-bind="{checked}"></slot>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+  data() {
+    return {
+      checked: false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .item {
+    color:red;
+  }
+</style>
